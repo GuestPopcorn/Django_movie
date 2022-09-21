2821bd45e647954fe469f147a7a9aa29c348d014
GuestPopcorn <abidabdurahim1@gmail.com>
2022-09-21 19:19:07 +0500

    17 урок

---
+++ movie/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+from .views import MoviesView, MovieDetailView, AddReview
+
+urlpatterns = [
+    path("", MoviesView.as_view()),
+    path("<slug:slug>/", MovieDetailView.as_view(), name='movie_detail'),
+    path("review/<int:pk>/", AddReview.as_view(), name='add_review'),
+
+]
